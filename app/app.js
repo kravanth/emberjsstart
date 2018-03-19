9a24457d53295420bfa1ec5602c5c09b1177e532
kravanth <kranthi.mg2@gmail.com>
2018-03-19 14:42:11 +0530

    new changes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import numericBox from './helpers/numeric_box';  // am:  l
+import Person from './helpers/person';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
@@ -10,5 +12,24 @@ const App = Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
+let textbox = numericBox.create();   // am:  lines added.
+console.log("number: ", textbox.valid("12"));
+textbox.onFocus();
+
+console.log("arithmetic: " ,textbox.valid("12 * 4"));  // false
+textbox.onFocus();
+
+console.log("string: " , textbox.valid("abc"));
+textbox.onFocus();
+
+
+let person = Person.create({
+  firstName: 'john',
+  lastName: 'miller'
+});
+
+// observer won't fire until `fullName` is consumed first
+person.get('fullName'); 
+person.set('firstName', 'Tony');
 
 export default App;
