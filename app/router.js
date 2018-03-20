4d5b85f89db955281eb73085fa989415193c05af
kravanth <kranthi.mg2@gmail.com>
2018-03-20 12:45:30 +0530

    validate email changes

--- app/router.js
+++ app/router.js
@@ -9,13 +9,16 @@ const Router = EmberRouter.extend({
 
   Router.map(function() {
     console.log("Router.map() @ Router.js") ;
+    this.route("home",{path: "/home"});
     this.route("about",{ path: "/about" });
    // this.route("twowaybind",{ path: "/twowaybind" });
     // this.route("products",{ path: "/products" });
    // this.route("product" ,{path:"/product"});  //alt way of defining route.
    this.route("products",function(){
     this.route("new");
+    
   });
+
     
   });
 
