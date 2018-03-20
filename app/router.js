c6006a3e600377f603420275187e743108f6ad4e
kravanth <kranthi.mg2@gmail.com>
2018-03-20 15:14:21 +0530

    routing change code

--- app/router.js
+++ app/router.js
@@ -10,13 +10,17 @@ const Router = EmberRouter.extend({
   Router.map(function() {
     console.log("Router.map() @ Router.js") ;
     this.route("home",{path: "/home"});
-    this.route("about",{ path: "/about" });
-   // this.route("twowaybind",{ path: "/twowaybind" });
-    // this.route("products",{ path: "/products" });
-   // this.route("product" ,{path:"/product"});  //alt way of defining route.
+   this.route("about",{ path: "/about" });
+   this.route("twowaybind",{ path: "/twowaybind" });
+   this.route("products",{ path: "/products" });
+   this.route("product" ,{path:"/product"});  //alt way of defining route.
    this.route("products",function(){
     this.route("new");
     
+  });
+  this.route("commits",function(){
+   
+    this.route("commit",{path:":sha"}); 
   });
 
     
